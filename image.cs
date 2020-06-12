dd08b51017afc32e28c3398d3bf8598c097baf5f
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2020-06-12 15:56:21 +0900

    Create image.cs

---
+++ image.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class image
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public image()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
