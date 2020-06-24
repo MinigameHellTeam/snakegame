048d856cc0113852349de8e96ae3c48a9d9d6ebc
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2020-06-24 22:47:54 +0900

    Update Settings.cs

--- Snake/Settings.cs
+++ Snake/Settings.cs
@@ -1,4 +1,4 @@
-﻿namespace Snake
+namespace Snake
 {
     public enum Direction
     {
@@ -10,23 +10,23 @@
 
     public class Settings
     {
-        public static int Width { get; set; }
-        public static int Height { get; set; }
+        public static bool GameOver { get; set; }
+        public static Direction direction { get; set; }
         public static int Speed { get; set; }
         public static int Score { get; set; }
         public static int Points { get; set; }
-        public static bool GameOver { get; set; }
-        public static Direction direction { get; set; }
+        public static int Life { get; set; }
+        
 
         public Settings()
         {
-            Width = 16;
-            Height = 16;
+            GameOver = false;
+            direction = Direction.Down;
             Speed = 10;
             Score = 0;
             Points = 100;
-            GameOver = false;
-            direction = Direction.Down;
+            Life = 3;
+            
         }
     }
 
