6e1795edb511c0b15c5bbfb974bef1da5757ae9d
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2020-06-24 22:47:15 +0900

    Update Form1.cs

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Reflection;
@@ -9,67 +9,71 @@ namespace Snake
     public partial class Form1 : Form
     {
         private List<image> Snake = new List<image>();
-        private Circle food = new Circle();
+        private image food = new image();
+        private image toxic = new image();
         private bool first = true;
+
 
         public Form1()
         {
             InitializeComponent();
             new Settings();
 
-            //게임속도, 타이머 세팅
             gameTimer.Interval = 1000 / Settings.Speed;
             gameTimer.Tick += UpdateScreen;
             gameTimer.Start();
             Settings.GameOver = true;
 
-            if (Settings.GameOver)
+            if (Settings.GameOver == true)
             {
-                //Check if Enter is pressed
                 if (Input.KeyPressed(Keys.Enter))
                 {
                     StartGame();
                 }
             }
-
         }
 
         private void StartGame()
         {
-            lblGameOver.Visible = false;
-           
+            lblState.Visible = false;
+            life_pic1.Visible = true;
+            life_pic2.Visible = true;
+            life_pic3.Visible = true;
 
-            //Set settings to default
             new Settings();
 
-            //Create new player object
             Snake.Clear();
-            image head = new image {X = 20, Y = 1};
+            image head = new image {X = 10, Y = 1};
             Snake.Add(head);
-
 
             lblScore.Text = Settings.Score.ToString();
             GenerateFood();
-
         }
 
-        //Place random food object
         private void GenerateFood()
         {
-            int maxXPos = pbCanvas.Size.Width / Settings.Width;
-            int maxYPos = pbCanvas.Size.Height / Settings.Height;
+            int xborder = pbCanvas.Size.Width / 32;
+            int yborder = pbCanvas.Size.Height / 32;
 
             Random random = new Random();
-            food = new Circle {X = random.Next(0, maxXPos), Y = random.Next(0, maxYPos)};
+            food = new image {X = random.Next(0, xborder), Y = random.Next(0, yborder) };
+            toxic = new image { X = random.Next(0, xborder), Y = random.Next(0, yborder) };
+        }
+
+        private void GenerateToxic()
+        {
+            int xborder = pbCanvas.Size.Width / 32;
+            int yborder = pbCanvas.Size.Height / 32;
+
+            Random random = new Random();
+            toxic = new image { X = random.Next(0, xborder), Y = random.Next(0, yborder) };
         }
 
 
         private void UpdateScreen(object sender, EventArgs e)
         {
-            //Check for Game Over
             if (Settings.GameOver)
             {
-                //Check if Enter is pressed
                 if (Input.KeyPressed(Keys.Enter))
                 {
                     StartGame();
@@ -96,14 +100,13 @@ namespace Snake
         private void pbCanvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics canvas = e.Graphics;
-            PictureBox img = new PictureBox();
-            
+            PictureBox snake_picture = new PictureBox();
+            PictureBox food_picture = new PictureBox();
+            PictureBox toxic_pircutre = new PictureBox();
+
 
             if (!Settings.GameOver)
             {
-                //Set color of snake
-
-                //Draw snake
                 
                 for (int i = 0; i < Snake.Count; i++)
                 {
@@ -113,57 +116,55 @@ namespace Snake
                         switch (Settings.direction)
                         {
                             case Direction.Down:
-                                img.Image = imglist.Images[1];
+                                snake_picture.Image = imglist.Images[1];
                                 break;
                             case Direction.Right:
-                                img.Image = imglist.Images[3];
+                                snake_picture.Image = imglist.Images[3];
                                 break;
                             case Direction.Left:
-                                img.Image = imglist.Images[2];
+                                snake_picture.Image = imglist.Images[2];
                                 break;
                             case Direction.Up:
-                                img.Image = imglist.Images[4];
+                                snake_picture.Image = imglist.Images[4];
                                 break;
                         }
                     }
                     else
                     {
-                        img.Image = imglist.Images[0];
+                        snake_picture.Image = imglist.Images[0];
                     }
-                    //Draw snake
-                    canvas.DrawImage(img.Image,
-                        new Rectangle(Snake[i].X * Settings.Width,
-                                      Snake[i].Y * Settings.Height,
-                                      Settings.Width, Settings.Height));
-                }
-               
-                    //Draw Food
-                    canvas.FillEllipse(Brushes.Red,
-                        new Rectangle(food.X * Settings.Width,
-                             food.Y * Settings.Height, Settings.Width, Settings.Height));
 
-                
+                    canvas.DrawImage(snake_picture.Image, new Rectangle(Snake[i].X * 32, Snake[i].Y * 32, 32, 32));
+                }
+
+
+                food_picture.Image = imglist.Images[5];
+                canvas.DrawImage(food_picture.Image, new Rectangle(food.X * 32,food.Y * 32, 32, 32));
+
+                toxic_pircutre.Image = imglist.Images[6];
+                canvas.DrawImage(toxic_pircutre.Image,new Rectangle(toxic.X * 32, toxic.Y * 32, 32, 32));
+
+
             }
             else if(first == false)
             {
-                string gameOver = "Game over \nYour final score is: " + Settings.Score + "\nPress Enter to try again";
-                lblGameOver.Text = gameOver;
-                lblGameOver.Visible = true;
+                lblState.Text = "Game over \nScore : " + Settings.Score + "\nPress Enter to start"; ;
+                lblState.Visible = true;
             }
             else
             {
-                string gamestart = "Press Enter to start";
-                lblGameOver.Text = gamestart;
-                lblGameOver.Visible = true;
+                lblState.Text = "Press Enter to start";
+                lblState.Visible = true;
             }
         }
 
 
         private void MovePlayer()
         {
+            int xborder = pbCanvas.Size.Width / 32;
+            int yborder = pbCanvas.Size.Height / 32;
             for (int i = Snake.Count - 1; i >= 0; i--)
             {
-                //Move head
                 if (i == 0)
                 {
                     switch (Settings.direction)
@@ -181,40 +182,44 @@ namespace Snake
                             Snake[i].Y++;
                             break;
                     }
+                   
 
-
-                    //Get maximum X and Y Pos
-                    int maxXPos = pbCanvas.Size.Width / Settings.Width;
-                    int maxYPos = pbCanvas.Size.Height / Settings.Height;
-
-                    //Detect collission with game borders.
                     if (Snake[i].X < 0 || Snake[i].Y < 0
-                        || Snake[i].X >= maxXPos || Snake[i].Y >= maxYPos)
+                        || Snake[i].X >= xborder || Snake[i].Y >= yborder)
                     {
+                        life_pic1.Visible = false;
+                        life_pic2.Visible = false;
+                        life_pic3.Visible = false;
                         Die();
                     }
 
 
-                    //Detect collission with body
                     for (int j = 1; j < Snake.Count; j++)
                     {
                         if (Snake[i].X == Snake[j].X &&
                            Snake[i].Y == Snake[j].Y)
                         {
+                            life_pic1.Visible = false;
+                            life_pic2.Visible = false;
+                            life_pic3.Visible = false;
                             Die();
                         }
                     }
 
-                    //Detect collision with food piece
+                    if (Snake[0].X == toxic.X && Snake[0].Y == toxic.Y)
+                    {
+                        decrease();
+                    }
+
                     if (Snake[0].X == food.X && Snake[0].Y == food.Y)
                     {
                         Eat();
                     }
 
+                  
                 }
                 else
                 {
-                    //Move body
                     Snake[i].X = Snake[i - 1].X;
                     Snake[i].Y = Snake[i - 1].Y;
                 }
@@ -233,19 +238,45 @@ namespace Snake
        
         private void Eat()
         {
-            //Add circle to body
-            image circle = new image
+            image food = new image
             {
                 X = Snake[Snake.Count - 1].X,
                 Y = Snake[Snake.Count - 1].Y
             };
-            Snake.Add(circle);
+            Snake.Add(food);
 
-            //Update Score
             Settings.Score += Settings.Points;
             lblScore.Text = Settings.Score.ToString();
-
             GenerateFood();
+        }
+        private void decrease()
+        {     
+            if(Snake.Count>1)
+            Snake.RemoveAt(Snake.Count - 1);
+
+            Settings.Life -= 1;
+            Settings.Score -= Settings.Points;
+            lblScore.Text = Settings.Score.ToString();
+
+            if (Settings.Life == 2)
+            {
+                life_pic1.Visible = false;
+            }
+            else if (Settings.Life == 1)
+            {
+                life_pic2.Visible = false;
+            }
+            else if (Settings.Life == 0)
+            {
+                life_pic3.Visible = false;
+            }
+
+            if (Settings.Life == 0)
+            {
+                Die();
+            }
+
+            GenerateToxic();
         }
 
         private void Die()
